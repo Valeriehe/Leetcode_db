078786084d994ba5642eba3f6d0bbac5fd7d92a6
Valeriehe <43585855+Valeriehe@users.noreply.github.com>
2018-12-12 10:53:02 -0500

    Create 610. Triangle Judgement.sql

---
+++ 610. Triangle Judgement.sql
@@ -0,0 +1,35 @@
+#### 610. Triangle Judgement(Easy) ####
+'''
+A pupil Tim gets homework to identify whether three line segments could possibly form a triangle.
+However, this assignment is very heavy because there are hundreds of records to calculate.
+Could you help Tim by writing a query to judge whether these three sides can form a triangle, assuming table triangle holds the length of the three sides x, y and z.
+| x  | y  | z  |
+|----|----|----|
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
+For the sample data above, your query should return the follow result:
+| x  | y  | z  | triangle |
+|----|----|----|----------|
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
+'''
+
+
+'''
+Tips:
+1) Case when (new col) end
+2) '' for string
+3) Test triangle: sum of 2 sides > the third side
+'''
+
+
+SELECT 
+    x, 
+    y, 
+    z,
+    (CASE 
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+     END) AS triangle
+FROM 
+    triangle
