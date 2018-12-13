aafec1186adce730db5a18319e4d5671c756007f
Valeriehe <43585855+Valeriehe@users.noreply.github.com>
2018-12-13 14:48:27 -0500

    Create 570. Managers with at Least 5 Direct Reports.sql

---
+++ 570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,40 @@
+#### 570. Managers with at Least 5 Direct Reports(Medium) ####
+'''
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++------+----------+-----------+----------+
+|Id    |Name 	  |Department |ManagerId |
++------+----------+-----------+----------+
+|101   |John 	  |A 	      |null      |
+|102   |Dan 	  |A 	      |101       |
+|103   |James 	  |A 	      |101       |
+|104   |Amy 	  |A 	      |101       |
+|105   |Anne 	  |A 	      |101       |
+|106   |Ron 	  |B 	      |101       |
++------+----------+-----------+----------+
+Given the Employee table, write a SQL query that finds out managers with at least 5 direct report. For the above table, your SQL query should return:
+
++-------+
+| Name  |
++-------+
+| John  |
++-------+
+Note:
+No one would report to himself.
+'''
+
+
+'''
+Tips:
+1) Use join, not left join
+'''
+
+
+SELECT 
+    E2.Name
+FROM 
+    Employee E1
+       JOIN 
+    Employee E2 ON E1.ManagerId = E2.Id
+GROUP BY E1.ManagerId
+HAVING COUNT(*) >= 5
