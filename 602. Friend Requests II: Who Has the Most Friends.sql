4b8f353907ec461fa96a0dce1baefa42456c13d0
Valeriehe <43585855+Valeriehe@users.noreply.github.com>
2018-12-29 16:50:23 -0500

    Update 602. Friend Requests II: Who Has the Most Friends.sql

--- 602. Friend Requests II: Who Has the Most Friends.sql
+++ 602. Friend Requests II: Who Has the Most Friends.sql
@@ -1,6 +1,6 @@
 ### 602. Friend Requests II: Who Has the Most Friends(Medium) ###
 
-'''
+"""
 In social network like Facebook or Twitter, people send friend requests and accept others' requests as well.
 Table request_accepted holds the data of friend acceptance, while requester_id and accepter_id both are the id of a person.
 | requester_id | accepter_id | accept_date|
@@ -20,7 +20,7 @@ Explanation:
 The person with id '3' is a friend of people '1', '2' and '4', so he has 3 friends in total, which is the most number than any others.
 Follow-up:
 In the real world, multiple people could have the same most number of friends, can you find all these people in this case?
-'''
+"""
 
 '''
 Tips:
