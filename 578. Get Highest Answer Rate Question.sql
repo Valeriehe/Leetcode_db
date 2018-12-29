4ea58fb85cc6d2d2773d1d7d9439a690d7175476
Valeriehe <43585855+Valeriehe@users.noreply.github.com>
2018-12-29 16:49:26 -0500

    Update 578. Get Highest Answer Rate Question.sql

--- 578. Get Highest Answer Rate Question.sql
+++ 578. Get Highest Answer Rate Question.sql
@@ -1,6 +1,6 @@
 #### 578. Get Highest Answer Rate Question(Medium) ####
 
-'''
+"""
 Get the highest answer rate question from a table survey_log with these columns: uid, action, question_id, answer_id, q_num, timestamp.
 
 uid means user id; action has these kind of values: "show", "answer", "skip"; answer_id is not null when action column is "answer", while is null for "show" and "skip"; q_num is the numeral order of the question in current session.
@@ -26,7 +26,7 @@ Output:
 Explanation:
 question 285 has answer rate 1/1, while question 369 has 0/1 answer rate, so output 285.
 Note: The highest answer rate meaning is: answer number's ratio in show number in the same question.
-
+"""
 
 ### Tips: ###
 '''
